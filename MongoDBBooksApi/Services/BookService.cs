0f824679351fcc2b438f0803fe3e11e6d164275b
derciosinione <derciosinione@gmail.com>
2021-12-24 16:47:38 +0100

    feat: Changed BookService

--- MongoDBBooksApi/Services/BookService.cs
+++ MongoDBBooksApi/Services/BookService.cs
@@ -19,5 +19,17 @@ namespace MongoDBBooksApi.Services
         public List<Book> Get() => _books.Find(book => true).ToList();
         
         public Book Get(string id) => _books.Find<Book>(book => book.Id == id).FirstOrDefault();
+        
+        public Book Create(Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+        
+        public void Update(string id, Book bookIn) => _books.ReplaceOne(book => book.Id == id, bookIn);
+
+        public void Remove(Book bookIn) => _books.DeleteOne(book => book.Id == bookIn.Id);
+
+        public void Remove(string id) => _books.DeleteOne(book => book.Id == id);
     }
 }
